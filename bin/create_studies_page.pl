9ab8b424f7235aa0007a1baa1d0ed6e960a5b907
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2019-01-07 12:16:52 +0000

    Update create studies script

--- bin/create_studies_page.pl
+++ bin/create_studies_page.pl
@@ -9,6 +9,7 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 use View::StudiesPage;
 use Model::Study;
+use Model::SkippedRuns;
 
 my ($species) = @ARGV;
 die "Usage: $0 species" unless $species;
@@ -21,5 +22,16 @@ find(
   },
   "$FindBin::Bin/../curation/studies/$species"
 );
-
-print View::StudiesPage->new($species, @studies)->to_html;
+my @skipped;
+find(
+  sub {
+    push @skipped, Model::SkippedRuns->from_folder($File::Find::name)
+      if -d $File::Find::name and $File::Find::name =~ /\w+\d+$/;
+  },
+  "$FindBin::Bin/../curation/skipped_runs/$species"
+);
+print View::StudiesPage->new($species, {
+  passing_checks => \@studies,
+  failing_checks => [],
+  skipped_runs => \@skipped,
+})->to_html;
