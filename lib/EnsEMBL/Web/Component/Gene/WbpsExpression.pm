2e7178db0f86adf47857db26624dd43082ab85ad
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2019-01-17 15:31:24 +0000

    Sketch out a new component

---
+++ lib/EnsEMBL/Web/Component/Gene/WbpsExpression.pm
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+
+# Copied as a single file from https://github.com/wormbase/wbps-expression into WBPS web code
+# Please no dependencies apart from those listed in ensembl-webcode cpanfile:
+# https://github.com/Ensembl/ensembl-webcode/blob/master/cpanfile
+
+package EnsEMBL::Web::Component::Gene::WbpsExpression;
+
+sub new {
+   # location of the data files to read from, passed in as config
+}
+
+sub get_data {
+  my ($species, $gene_id) = @_;
+  # look through the files, provide objects the UI can render
+  
+
+  # return format documented clearly
+  return {};
+}
+
+1;
