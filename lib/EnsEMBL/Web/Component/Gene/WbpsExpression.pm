a65ac8770b66d5714bf77890d789e19534f00efb
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2019-02-05 13:46:59 +0000

    Prototype the gene page a bit more

--- lib/EnsEMBL/Web/Component/Gene/WbpsExpression.pm
+++ lib/EnsEMBL/Web/Component/Gene/WbpsExpression.pm
@@ -6,21 +6,61 @@ use warnings;
 # https://github.com/Ensembl/ensembl-webcode/blob/master/cpanfile
 
 package EnsEMBL::Web::Component::Gene::WbpsExpression;
+use File::Basename;
 
 sub new {
-   # location of the data files to read from, passed in as config
+  my ($class, $args) = @_;
+  return bless $args, $class;
 }
 sub from_folder {
-
+  my ($species, $assembly, $studies_path) = @_;
+  my $studies_file = "$studies_path/$species.$assembly.studies.tsv";
+  my %metadata;
+  open (my $fh, "<", $studies_file) or return;
+  while(<$fh>){
+    chomp;
+    my ($accession, $title) = split "\t";
+    $metadata{$accession}{title} = $title;
+  }
+  my @studies;
+  for my $study_path (grep {-d $_}  glob("$studies_path/*")){
+     my $study_id = basename $study_path;
+     push @studies, {
+        tpms_per_condition => "$study_path/$study_id.tpm.tsv",
+        study_id => $study_id,
+        study_title => $metadata{$study_id}{title},
+     };
+  }
+  return &new(__PACKAGE__, {studies => \@studies});
 }
 
 sub get_data {
-  my ($species, $gene_id) = @_;
-  # look through the files, provide objects the UI can render
-  
-
-  # return format documented clearly
-  return {};
+  my ($self, $gene_id) = @_;
+  my @result;
+  for my $study (@{$self->{studies}}){
+     my ($conditions, $expression_tpm) = search_in_file($study->{tpms_per_condition}, $gene_id);
+     push @result, {
+       study_id => $study->{study_id},
+       study_title => $study->{study_title},
+       conditions => $conditions,
+       expression_tpm => $expression_tpm,
+     } if $conditions and $expression_tpm;
+  }
+  return \@result;;
 }
 
+sub search_in_file {
+   my ($path, $gene_id) = @_;
+   my $l = `grep --max-count=1 "^$gene_id" $path`;
+   chomp $l;
+   return unless $l;
+   my ($id, @xs) = split "\t", $l;
+   return unless $id eq $gene_id and @xs;
+   my $h = `grep --max-count=1 "^\t" $path`;
+   chomp $h;
+   return unless $h;
+   my ($blank, @hs) = split "\t", $h;
+   return unless not($blank) and @hs;
+   return \@hs, \@xs;
+}
 1;
--- t/gene_page.t
+++ t/gene_page.t
@@ -1,31 +1,49 @@
-
+use strict;
+use warnings;
 use EnsEMBL::Web::Component::Gene::WbpsExpression;
-use Test::More skip_all => "Not implemented yet";
+use Test::More;
 use File::Temp qw/tempdir/;
 use File::Slurp qw/write_file/;
+use File::Path qw/make_path/;
 
 my $dir = tempdir(CLEANUP => 1);
-my $f1 = <<EOF
+my $f1 = <<EOF;
 #temp file
 	heads	tails
 g1	1.1	1.2
-g2	2.1	2.1
+g2	2.1	2.2
 EOF
 my $study_id = "SRP071241";
+my $study_title = "Comparison of gene expression between female Schistosoma mansoni heads and tails";
 my $f1_name = "$study_id.tpm.tsv";
-write_file(join("/", $dir, $study_id, $f1_name), $f1);
+my $f1_path = join("/", $dir, $study_id, $f1_name);
+
+my $species = "schistosoma_mansoni";
+my $assembly = "Smansoni_v7";
+make_path(join("/", $dir, $study_id));
+write_file($f1_path, $f1);
+write_file(join("/", $dir, "$species.$assembly.studies.tsv"), "$study_id\t$study_title\n");
 
 my $subject = EnsEMBL::Web::Component::Gene::WbpsExpression::from_folder(
-   $dir
+   $species, $assembly, $dir
 );
 
 is_deeply($subject, bless({
   studies => [{
-     accession => "$study_id",
-     title => "Comparison of gene expression between female Schistosoma mansoni heads and tails",
-     tpms_per_condition => join("/", $study_id, $f1_name),
+     study_id => "$study_id",
+     study_title => $study_title,
+     tpms_per_condition => $f1_path, 
   }]
 }, 'EnsEMBL::Web::Component::Gene::WbpsExpression'), "Create reads in the config");
+
+is_deeply($subject->get_data("invalid ID"), [], "Null case");
+
+is_deeply($subject->get_data("g2"), [{
+  study_id => $study_id,
+  study_title => $study_title,
+  conditions => ["heads", "tails"],
+  expression_tpm => [2.1, 2.2],
+}] , "One line");
 
 
 done_testing;
