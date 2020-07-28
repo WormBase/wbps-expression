2b665b15a1660c25239e0c777e0313a765176e0b
Tim Stickland <t.r.stickland@gmail.com>
2020-07-28 14:24:56 +0100

    switch PubMed download to E-utilities (NLM official API, https://dataguide.nlm.nih.gov/eutilities/how_eutilities_works.html) so proper XML is downloaded, and the parsing is more reliable. Add some basic parse assertions (expected alements must exist).

--- lib/WbpsExpression/IncomingStudies/StudyMetadata/PubMedClient.pm
+++ lib/WbpsExpression/IncomingStudies/StudyMetadata/PubMedClient.pm
@@ -6,41 +6,56 @@ use List::MoreUtils qw(uniq);
 use LWP;
 use XML::Simple;
 use Log::Any '$log';
+use Try::Tiny;
+use Carp;
 
 sub title_and_description_for_pubmed_id {
   my ( $species, $pubmed_id ) = @_;
   my $url =
-    "https://www.ncbi.nlm.nih.gov/pubmed/$pubmed_id?report=xml&format=text";
-
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=$pubmed_id&retmode=xml";
   $log->info("PubMedClient get_xml LWP::get $url");
   my $response = LWP::UserAgent->new->get($url);
   die "$url error:" . $response->status_line . "\n"
     unless $response->is_success;
-  my $payload_string = XMLin( $response->decoded_content );
+
+  my $payload_string = $response->decoded_content;
   return unless $payload_string; #rare but e.g. 30962434
-  my $payload        = XMLin($payload_string);
 
-# Use regex because XML::Simple is being too simple and creates too much structure
-# E.g. 30049782: <ArticleTitle> Stuff in <i>Caenorhabditis elegans</i>.</ArticleTitle>
-  my ($title) = $payload_string =~ m{<ArticleTitle>(.*)</ArticleTitle>};
-  $title = italicise_species_in_title( $species, $title );
-
-  my $authors = $payload->{MedlineCitation}{Article}{AuthorList}{Author};
-  my @authors =
-    $authors ? ref $authors eq 'ARRAY' ? @$authors : ($authors) : ();
-  my $first_author = $authors[0]->{LastName};
-  my $last_author  = $authors[-1]->{LastName};
-  $authors =
-      $first_author
-    ? $last_author ne $first_author
-      ? "$first_author .. $last_author"
-      : $first_author
-    : "";
-  my $year =
-    $payload->{MedlineCitation}{Article}{Journal}{JournalIssue}{PubDate}{Year};
-  my $short_description = "$authors, $year";
-  my $full_description =
-    $title ? "$title ($authors, $year)" : $short_description;
+  my ($short_description, $full_description );
+  try {
+    my $payload = XMLin($payload_string);
+  
+    my $article_elt = $payload->{PubmedArticleSet}{PubmedArticle}{MedlineCitation}{Article}
+      || die "cannot find PubmedArticleSet|PubmedArticle|MedlineCitation|Article";
+    
+    my $title = $article_elt->{ArticleTitle}
+      || die "cannot find PubmedArticleSet|PubmedArticle|MedlineCitation|Article|ArticleTitle";
+    $title = italicise_species_in_title( $species, $title );
+  
+    my $authors = $article_elt->{AuthorList}{Author}
+      || die "cannot find PubmedArticleSet|PubmedArticle|MedlineCitation|Article|AuthorList|Author";
+    my @authors =
+      $authors ? ref $authors eq 'ARRAY' ? @$authors : ($authors) : ();
+    my $first_author = $authors[0]->{LastName};
+    my $last_author  = $authors[-1]->{LastName};
+    $authors =
+        $first_author
+      ? $last_author ne $first_author
+        ? "$first_author .. $last_author"
+        : $first_author
+      : "";
+    my $year = $article_elt->{Journal}{JournalIssue}{PubDate}{Year}
+      || die "cannot find PubmedArticleSet|PubmedArticle|MedlineCitation|Article|Journal|JournalIssue|PubDate|Year";
+    $short_description = "$authors, $year";
+    $full_description =
+      $title ? "$title ($authors, $year)" : $short_description;
+  } catch {
+    my $msg = $_;
+    my $xmllog = "/tmp/pubmid-$pubmed_id-$$.xml";
+    write_file($xmllog, $payload_string);
+    confess "PubMed XML parsing error (XML saved as $xmllog): $msg";
+  }
+    
   return [ $short_description, $full_description ];
 }
 
